187ebf70310f87ba4c972216fb478c323ac26c53
TungDaiCa <shane92mai@gmail.com>
2020-01-16 16:43:38 +0700

    tung update

---
+++ hen_ho/dao/src/main/java/com/codegym/dao/entity/Role.java
@@ -0,0 +1,35 @@
+package com.codegym.dao.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="role")
+public class Role {
+    @Id
+    @Column(name = "id_role")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idRole;
+
+    @Column(name = "role_name")
+    private String roleName;
+
+    public int getIdRole() {
+        return idRole;
+    }
+
+    public void setIdRole(int idRole) {
+        this.idRole = idRole;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Role(String roleName) {
+        this.roleName = roleName;
+    }
+}
---
+++ hen_ho/dao/src/main/java/com/codegym/dao/entity/ThongBao.java
@@ -0,0 +1,48 @@
+package com.codegym.dao.entity;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name="thong_bao")
+public class ThongBao {
+    @Id
+    @Column(name = "id_thong_bao")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idThongBao;
+
+    @Column(name = "noi_dung")
+    private String noiDung;
+
+    @Column(name = "thoi_gian_thong_bao")
+    private Date thoiGianThongBao;
+
+    public int getIdThongBao() {
+        return idThongBao;
+    }
+
+    public void setIdThongBao(int idThongBao) {
+        this.idThongBao = idThongBao;
+    }
+
+    public String getNoiDung() {
+        return noiDung;
+    }
+
+    public void setNoiDung(String noiDung) {
+        this.noiDung = noiDung;
+    }
+
+    public Date getThoiGianThongBao() {
+        return thoiGianThongBao;
+    }
+
+    public void setThoiGianThongBao(Date thoiGianThongBao) {
+        this.thoiGianThongBao = thoiGianThongBao;
+    }
+
+    public ThongBao(String noiDung, Date thoiGianThongBao) {
+        this.noiDung = noiDung;
+        this.thoiGianThongBao = thoiGianThongBao;
+    }
+}
