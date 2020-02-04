20ecf429154c49e04198ab9683cc166658dd673b
https://github.com/trungnam95/C0919G1_Module2_NguyenTrungNam.git <nguyentrungnam100495@gmail.com>
2020-02-04 15:04:55 +0700

    update

--- hen_ho/service/src/main/java/com/codegym/service/HoiNhomService.java
+++ hen_ho/service/src/main/java/com/codegym/service/HoiNhomService.java
@@ -1,6 +1,8 @@
 package com.codegym.service;
 
+import com.codegym.dao.DTO.DetailHoiNhomDTO;
 import com.codegym.dao.entity.HoiNhom;
+import com.codegym.dao.entity.NhomThanhVien;
 
 import java.util.List;
 
@@ -9,4 +11,6 @@ public interface HoiNhomService {
     HoiNhom findById(Long id);
     void save(HoiNhom hoiNhom);
     void remove(Long id);
+//    DetailHoiNhomDTO findById(Long id);
+
 }
